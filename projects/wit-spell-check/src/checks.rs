627fe6325b0304c6ea75a424b1000075575a159d
Aster <aster@vers.site>
2024-02-28 23:30:03 +0800

    📝 Detect function typos in preview2

--- projects/wit-spell-check/src/checks.rs
+++ projects/wit-spell-check/src/checks.rs
@@ -6,26 +6,39 @@ use wit_parser::{TypeDef, UnresolvedPackage};
 
 pub struct WitSpellCheck {
     hunspell: Hunspell,
-    accept: BTreeSet<Cow<'static, str>>,
-    reject: BTreeSet<Cow<'static, str>>,
+    check_function: bool,
+    check_parameter: bool,
 }
 
-impl Default for WitSpellCheck {
-    fn default() -> Self {
-        let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("data/en_US");
+impl WitSpellCheck {
+    pub fn new<P>(directory: P) -> Self where P: AsRef<Path> {
+        let dir = directory.as_ref();
+        let aff = dir.join("en_US.aff");
+        let dic = dir.join("en_US.dic");
+        let mut hun = Hunspell::new(&aff.to_string_lossy(), &dic.to_string_lossy());
+        hun.add_dictionary(&dir.join("custom.dic").to_string_lossy());
         Self {
-            hunspell: Hunspell::new(
-                path.join("en_US.aff").to_str().unwrap(),
-                path.join("en_US.dic").to_str().unwrap(),
-            ),
-            accept: Default::default(),
-            reject: Default::default(),
+            hunspell: hun,
+            check_function: true,
+            check_parameter: true,
+        }
+    }
+    pub fn check_functions(self, check: bool) -> Self {
+        Self {
+            check_function: check,
+            ..self
+        }
+    }
+    pub fn check_parameters(self, check: bool) -> Self {
+        Self {
+            check_parameter: check,
+            ..self
         }
     }
 }
 
 impl WitSpellCheck {
-    pub fn check<P: AsRef<Path>>(&self, directory: P) {
+    pub fn check<P>(&self, directory: P) where P: AsRef<Path> {
         let package = UnresolvedPackage::parse_dir(directory.as_ref()).unwrap();
         for (_, interface) in package.interfaces.iter() {
             for (name, function) in interface.functions.iter() {
@@ -43,10 +56,26 @@ impl WitSpellCheck {
                                     None => {}
                                 }
                                 println!("  - in package `{}`", package.name);
-                                println!("  - perhaps");
-                                for suggestion in self.hunspell.suggest(part) {
-                                    println!("    - `{}`", suggestion);
+                                println!("  - perhaps {:?}", self.hunspell.suggest(part));
+                            }
+                        }
+                    }
+                }
+                let trim = name.trim_start_matches("[constructor]").trim_start_matches("[static]").trim_start_matches("[method]");
+                for split in trim.split(".") {
+                    for part in split.split("-") {
+                        match self.hunspell.check(part) {
+                            CheckResult::FoundInDictionary => {}
+                            CheckResult::MissingInDictionary => {
+                                println!("- function may wrong: `{}`", part);
+                                match &interface.name {
+                                    Some(s) => {
+                                        println!("  - in interface `{}`", s);
+                                    }
+                                    None => {}
                                 }
+                                println!("  - in package `{}`", package.name);
+                                println!("  - perhaps {:?}", self.hunspell.suggest(part));
                             }
                         }
                     }
