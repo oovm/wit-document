6fab775b0eec5ff8abfb4f667a2fa176549421fc
Aster <aster@vers.site>
2024-02-28 22:50:41 +0800

    Add spell checker

--- projects/wit-document/src/renderer/mod.rs
+++ projects/wit-document/src/renderer/mod.rs
@@ -1,16 +1,18 @@
-use crate::DataProvider;
-use dioxus::{html::s, prelude::*};
-use dioxus::html::KeyCode::N;
+use crate::{
+    helpers::{DocumentElement, DocumentElementIcon},
+    DataProvider,
+};
+use dioxus::{
+    html::{s, KeyCode::N},
+    prelude::*,
+};
 use indexmap::IndexMap;
 use wit_parser::{Function, Interface, TypeDef, TypeDefKind, TypeId, World};
-use crate::helpers::DocumentElementIcon;
-use crate::helpers::DocumentElement;
 
-
-mod render_types;
-mod render_interface;
-mod render_world;
 mod render_functions;
+mod render_interface;
+mod render_types;
+mod render_world;
 
 pub fn render_interface(data: &DataProvider, interface: &Interface) -> Element {
     let words = data.package.worlds.iter().map(|(key, value)| make_link(value, data, "left-link"));
@@ -28,7 +30,7 @@ pub fn render_interface(data: &DataProvider, interface: &Interface) -> Element {
 }
 
 fn make_link<T: DocumentElement + DocumentElementIcon>(item: &T, data: &DataProvider, class: &'static str) -> Element {
-    match item.get_name(data) {
+    match item.get_name(data).as_ref() {
         "" => rsx! {},
         name => {
             let link = item.get_link(data);
@@ -43,16 +45,32 @@ fn make_link<T: DocumentElement + DocumentElementIcon>(item: &T, data: &DataProv
         }
     }
 }
-
+fn main_link<T: DocumentElement + DocumentElementIcon>(item: &T, data: &DataProvider) -> Element {
+    match item.get_name(data).as_ref() {
+        "" => rsx! {},
+        name => {
+            let link = item.get_link(data);
+            let icon = item.get_icon_name();
+            rsx! {
+               td {
+                    class: "main-link",
+                    span { class: "type-icon", "{icon}" }
+                    a { href: "{link}", "{name}" }
+                }
+            }
+        }
+    }
+}
 fn main_resources<'a>(data: &'a DataProvider, item: &'a IndexMap<String, TypeId>) -> Element {
     let title = if data.has_resources(item) {
         rsx! {
-                 h2 {
-                    id: "resources",
-                    "Resources"
-                }
+             h2 {
+                id: "resources",
+                "Resources"
             }
-    } else {
+        }
+    }
+    else {
         None
     };
     let terms = data.get_resources(item).into_iter().map(|x| x.main_card(data));
@@ -64,23 +82,25 @@ fn main_resources<'a>(data: &'a DataProvider, item: &'a IndexMap<String, TypeId>
     }
 }
 
-
 fn main_functions<'a>(data: &'a DataProvider, item: &'a IndexMap<String, Function>) -> Element {
     let title = if data.has_functions(item) {
         rsx! {
-                 h2 {
-                    id: "functions",
-                    "Functions"
-                }
+             h2 {
+                id: "functions",
+                "Functions"
             }
-    } else {
+        }
+    }
+    else {
         None
     };
     let terms = data.get_functions(item).into_iter().map(|x| x.main_card(data));
     rsx! {
         Fragment {
             {title}
-            {terms}
+            table {
+                {terms}
+            }
         }
     }
 }
--- projects/wit-document/src/renderer/render_types.rs
+++ projects/wit-document/src/renderer/render_types.rs
@@ -1,24 +1,24 @@
-use crate::helpers::DocumentElement;
 use super::*;
-
+use crate::helpers::DocumentElement;
+use std::borrow::Cow;
 
 impl DocumentElementIcon for TypeDefKind {
     fn get_icon_name(&self) -> char {
         match self {
-            Self::Record(_) => { 'S' }
-            Self::Resource => { 'R' }
-            Self::Handle(_) => { 'T' }
-            Self::Flags(_) => { 'F' }
-            Self::Tuple(_) => { 'T' }
-            Self::Variant(_) => { 'U' }
-            Self::Enum(_) => { 'E' }
-            Self::Option(_) => { 'T' }
-            Self::Result(_) => { 'T' }
-            Self::List(_) => { 'T' }
-            Self::Future(_) => { 'T' }
-            Self::Stream(_) => { 'T' }
-            Self::Type(_) => { 'T' }
-            Self::Unknown => { '?' }
+            Self::Record(_) => 'S',
+            Self::Resource => 'R',
+            Self::Handle(_) => 'T',
+            Self::Flags(_) => 'F',
+            Self::Tuple(_) => 'T',
+            Self::Variant(_) => 'U',
+            Self::Enum(_) => 'E',
+            Self::Option(_) => 'T',
+            Self::Result(_) => 'T',
+            Self::List(_) => 'T',
+            Self::Future(_) => 'T',
+            Self::Stream(_) => 'T',
+            Self::Type(_) => 'T',
+            Self::Unknown => '?',
         }
     }
 }
@@ -29,12 +29,11 @@ impl DocumentElementIcon for TypeDef {
     }
 }
 
-
 impl DocumentElement for TypeDef {
-    fn get_name(&self, _: &DataProvider) -> &str {
+    fn get_name(&self, _: &DataProvider) -> Cow<str> {
         match self.name.as_ref() {
-            Some(name) => name,
-            None => ""
+            Some(name) => Cow::Borrowed(name),
+            None => Cow::Borrowed(""),
         }
     }
 
@@ -50,12 +49,10 @@ impl DocumentElement for TypeDef {
         match self.kind {
             TypeDefKind::Record(_) => panic!(),
             TypeDefKind::Resource => {
-                let link = make_link(self, data, "main-link");
+                let link = main_link(self, data);
                 let document = match &self.docs.contents {
-                    None => { "" }
-                    Some(document) => {
-                        document.lines().next().unwrap_or("")
-                    }
+                    None => "",
+                    Some(document) => document.lines().next().unwrap_or(""),
                 };
                 rsx! {
                     tr {
@@ -84,6 +81,4 @@ impl DocumentElement for TypeDef {
             TypeDefKind::Unknown => panic!(),
         }
     }
-
-
 }
